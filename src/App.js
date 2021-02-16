a2196b332bf842391cadaf38604b2bc274ea1fc2
oleg <litwinenkooleg371@gmail.com>
2021-02-16 19:44:40 +0200

    первая проверка

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Panel from "./components/Panel";
 import paintings from "./paintings.json";
 
 
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+
 const App = () => {
     return (
         <div>
